4322836b6ba2da73cfe2fd7825228f826e036ce5
Gabor Greif <ggreif@gmail.com>
2015-08-18 16:42:09 -0300

    primitive signal fetcher
    *Main> sampleN 2 $ fetch $ register 0 (fromList (cycle [1,0]))
    [Alu Add 4 5,Stop]

--- ALU.hs
+++ ALU.hs
@@ -4,7 +4,6 @@
 import CLaSH.Prelude
 import ALUTypes
 
---data OP = Add | Sub
 
 alu :: OP -> Signed 9 -> Signed 9 -> Signed 9
 alu Add a1 a2 = a1 + a2
@@ -18,7 +17,13 @@ topEntity = liftA3 alu
 
 data Instr = Alu OP (Signed 9) (Signed 9)
            | Stop
+  deriving Show
 
+
+fetch :: Signal Word -> Signal Instr
+fetch = liftA go where
+   go 0 = Alu Add 4 5
+   go 1 = Stop
 
 
 testInput :: Signal (OP,Signed 9,Signed 9)
