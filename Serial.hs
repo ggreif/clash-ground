1fb73028470602dab0c42df9dee3b098c8c333ca
Gabor Greif <ggreif@gmail.com>
2015-08-19 17:23:15 -0300

    this actually generates VHDL now

--- Serial.hs
+++ Serial.hs
@@ -5,11 +5,13 @@ import CLaSH.Prelude
 data Chopped = O | I | Done
   deriving (Show, Read, Bounded, Eq, Ord, Enum)
 
-chop :: Enum a => Signal a -> Signal Chopped
+--chop :: Enum a => Signal a -> Signal Chopped
+chop :: Signal (Unsigned 24) -> Signal Chopped
 chop = liftA go where
-  go (fromEnum -> 0) = Done
-  go n = toEnum (fromEnum n .&. 0b1)
-
+  go 0 = Done
+  go n = toChopped (n .&. 0b1)
+  toChopped 0 = O
+  toChopped 1 = I
 
 topEntity :: Signal (Unsigned 24) -> Signal Chopped
 topEntity = chop
