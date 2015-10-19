48f2afeb84c8cd98ab1a0180dd0470c0c1122214
ggreif <ggreif@gmail.com>
2015-10-20 01:15:21 +0200

    mask tag bits:
    *Arena> L.drop 1 $ sampleN 30 $ topEntity (signal 7)
    [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4]

--- Arena.hs
+++ Arena.hs
@@ -8,14 +8,16 @@ import Data.Proxy
 
 type BitWidth = 5
 
-load :: (KnownNat n, KnownNat tags) => SNat n -> SNat tags -> Signal (Unsigned BitWidth) -> Signal (Unsigned BitWidth)
-load _ _ addr = blockRamPow2 init addr addr read (signal 0) where
+load :: SNat n -> SNat tags -> Signal (Unsigned BitWidth) -> Signal (Unsigned BitWidth)
+--load :: (KnownNat n, KnownNat tags) => SNat n -> SNat tags -> Signal (Unsigned BitWidth) -> Signal (Unsigned BitWidth)
+load _ tags addr = blockRamPow2 init masked masked read (signal 0) where
     init = 0 :> 1 :> 2 :> 3 :> 4 :> 5 :> 6 :> 7 :> 8 :> 9 :> 10 :> 11 :> 12 :> 13 :> 14 :> 15 :>
            0 :> 1 :> 2 :> 3 :> 4 :> 5 :> 6 :> 7 :> 8 :> 9 :> 10 :> 11 :> 12 :> 13 :> 14 :> 15 :> Nil
     read = signal False
-    -- mask 
+    -- masked :: Signal (Unsigned BitWidth)
+    masked = (.&. complement (shiftL 1 (fromEnum tags) - 1)) <$> addr
 
-topEntity = load d3 d1
+topEntity = load d3 d2
 
 instance Eq (SNat n) where
   _ == _ = True
