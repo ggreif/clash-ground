df749b8bb96b3d1ff24cca77c051628f3f588db2
ggreif <ggreif@gmail.com>
2015-10-20 20:56:50 +0200

    begin supplying principal data along with metadata

--- Arena.hs
+++ Arena.hs
@@ -8,15 +8,17 @@ import Data.Proxy
 
 type BitWidth = 5
 
-loadMeta :: SNat n -> SNat tags -> Signal (Unsigned BitWidth) -> Signal (Unsigned BitWidth)
-loadMeta n tags addr = blockRamPow2 init shifted shifted read (signal 0) where
+load :: SNat n -> SNat tags -> Signal (Unsigned BitWidth) -> Signal (Unsigned BitWidth, Unsigned BitWidth)
+load n tags addr = (,) <$> orig <*> meta where
     init = 0 :> 1 :> 2 :> 3 :> 4 :> 5 :> 6 :> 7 :> 8 :> 9 :> 10 :> 11 :> 12 :> 13 :> 14 :> 15 :>
            0 :> 1 :> 2 :> 3 :> 4 :> 5 :> 6 :> 7 :> 8 :> 9 :> 10 :> 11 :> 12 :> 13 :> 14 :> 15 :> Nil
     read = signal False
     masked = (.&. complement (shiftL 1 (fromEnum tags) - 1)) <$> addr
     shifted = flip shiftR (fromEnum n) <$> masked
+    meta = blockRamPow2 init shifted shifted read (signal 0) -- too big blockRam
+    orig = blockRamPow2 init masked masked read (signal 0)
 
-topEntity = loadMeta d3 d2
+topEntity = load d3 d2
 
 testInput :: Signal (Unsigned BitWidth)
 testInput = stimuliGenerator $ 0 :> 1 :> 2 :> 3 :> 4 :> 5 :> 6 :> 7 :> 8 :> 9 :> 10 :> 11 :> 12 :> 13 :> 14 :> 15 :> 16 :> 17 :> 18 :> 19 :> 20 :> 21 :> 22 :> 23 :> 24 :> 25 :> 26 :> 27 :> 28 :> 29 :> 30 :> 31 :> 32 :> 33 :> 34 :> Nil
