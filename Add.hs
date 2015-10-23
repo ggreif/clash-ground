9d2bf8c2c37daf9e5e90c5d6546cf66908e9b79b
ggreif <ggreif@gmail.com>
2015-10-24 00:48:51 +0200

    simplify

--- Add.hs
+++ Add.hs
@@ -35,25 +35,30 @@ type DBI = Unsigned DB
 data Instr = LOAD W  -- absolute
            | WRITE W -- upmost row
            | BRANCH DBI
+           | LEFT DBI
+           | RIGHT DBI
            | NOP
            | STOP
            | RE
-           | AD Bool DBI
+           | AD DBI
 
 
 type DeBruijn = Vec DB
 
+dbi :: Vec (DB-1) a -> DBI -> a
+dbi = (!!)
+
 redox :: DeBruijn (W, W, W) -> Instr -> (DeBruijn (W, W, W), Maybe W)
-redox ((a, b, c):>(m:<l)) (LOAD addr) = (l:>(addr, b, c):>m, Nothing)
+redox ((_, b, c):>(m:<l)) (LOAD addr) = (l:>(addr, b, c):>m, Nothing)
 redox (h:>(m:<l)) NOP = (l:>h:>m, Nothing)
-redox ((a,b,c):>m) (AD False dbi) = ((a+d,b+e,c+f):>m, Nothing)
-  where (d,e,f) = m !! dbi
+redox ((a,b,c):>m) (AD (dbi m -> (d,e,f))) = ((a+d,b+e,c+f):>m, Nothing)
+  --where (d,e,f) = m !! dbi
 redox (h:>m) RE = (m:<h, Nothing)
 redox v STOP = (v, theLook v)
   where theLook ((a, _, _):>_) = Just a
 
 topEntity = mealy redox (repeat (0,0,0))
 
-testInput = stimuliGenerator $ LOAD 11 :> LOAD 3 :> RE :> AD False 0 :> STOP :> Nil
+testInput = stimuliGenerator $ LOAD 11 :> LOAD 3 :> RE :> AD 0 :> STOP :> Nil
 
 t = sampleN 30 $ topEntity testInput
