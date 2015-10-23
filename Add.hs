8f1f776f9f64f1b89f5914a4f94bf8b98aa39dae
ggreif <ggreif@gmail.com>
2015-10-23 23:04:03 +0200

    we have a very primitive machine

--- Add.hs
+++ Add.hs
@@ -25,3 +25,26 @@ exec :: CONT k -> Int -> k
 exec (ADD c a) (exec c . (a+) -> res) = res
 exec (NEXT c) (eval . (ADD c) -> res) = res
 exec HALT a = a
+
+
+type W = Unsigned 10
+type DBI = Unsigned 16
+
+data Instr = LOAD W
+           | WRITE W -- upmost row
+           | BRANCH DBI
+           | NOP
+           | STOP
+
+
+type InstrS = Signal Instr
+
+redox :: Vec 3 W -> Instr -> (Vec 3 W, Bool)
+redox (a:>b:>c:>Nil) (LOAD addr) = (b:>c:>addr:>Nil, False)
+redox v STOP = (v, True)
+
+topEntity = mealy redox (repeat 0)
+
+testInput = stimuliGenerator $ LOAD 11 :> LOAD 3 :> STOP :> Nil
+
+t = sampleN 30 $ topEntity testInput
