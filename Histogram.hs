f9bdfded1ae8ecf0a4962d444990842acbddc0c1
ggreif <ggreif@gmail.com>
2015-12-12 00:39:17 +0100

    tweaks for readability

--- Histogram.hs
+++ Histogram.hs
@@ -4,9 +4,9 @@ import qualified Data.List as L
 histo :: (KnownNat n, KnownNat (2 ^ n), KnownNat b) => Signal (Unsigned n) -> Signal (Unsigned b)
 histo nums = read
   where init = repeat 0
-        delayedWr = False `register` signal True
+        wrEnable = False `register` signal True
+        read = blockRamPow2 init wrAddr nums wrEnable write
         write = read + 1
-        read = blockRamPow2 init wrAddr nums delayedWr write
         wrAddr = 0 `register` nums
 
 -- #### TEST BENCH ####
@@ -19,7 +19,7 @@ testInput :: Signal (Unsigned 7)
 testInput = stimuliGenerator $ 1 :> 2 :> 0 :> 2 :> 3 :> 0 :> 3 :> 0 :> 3 :> 4 :> 0 :> 1 :> 0 :> Nil
 
 expectedOutput :: Signal (Unsigned 10) -> Signal Bool
-expectedOutput = outputVerifier $ undefined :>
-                               0 :> 0 :> 0 :> 1 :> 0 :> 1 :> 1 :> 2 :> 2 :> 0 :> 3 :> 1 :> 4 :> Nil
+expectedOutput = outputVerifier $
+                  undefined :> 0 :> 0 :> 0 :> 1 :> 0 :> 1 :> 1 :> 2 :> 2 :> 0 :> 3 :> 1 :> 4 :> Nil
 
 test = L.takeWhile not . L.drop 1 . sample $ expectedOutput (topEntity testInput)
