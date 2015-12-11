7d12018a1d66c1ba5da467745b64fcfbc0de8459
ggreif <ggreif@gmail.com>
2015-12-11 23:24:07 +0100

    add a test bench
    (doesn't pass yet)

--- Histogram.hs
+++ Histogram.hs
@@ -1,6 +1,5 @@
 import CLaSH.Prelude
-
-
+import qualified Data.List as L
 
 histo :: (KnownNat n, KnownNat (2 ^ n), KnownNat b) => Signal (Unsigned n) -> Signal (Unsigned b)
 histo nums = read
@@ -10,5 +9,16 @@ histo nums = read
         read = blockRamPow2 init nums wrAddr delayedWr write
         wrAddr = 0 `register` nums
 
+-- #### TEST BENCH ####
+
+topEntity :: Signal (Unsigned 7) -> Signal (Unsigned 10)
+topEntity = histo
 
 
+testInput :: Signal (Unsigned 7)
+testInput = stimuliGenerator $ 1 :> 2 :> 2 :> 3 :> 3 :> 3 :> 4 :> 0 :> 1 :> Nil
+
+expectedOutput :: Signal (Unsigned 10) -> Signal Bool
+expectedOutput = outputVerifier $ 0 :> 0 :> 1 :> 0 :> 1 :> 2 :> 0 :> 0 :> 1 :> Nil
+
+test = L.drop 1 $ sampleN 30 $ expectedOutput (topEntity testInput)
