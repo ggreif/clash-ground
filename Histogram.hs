f8467a109445431d542577c3c7b9ebe2fea444c9
ggreif <ggreif@gmail.com>
2015-12-11 23:51:37 +0100

    delay the read value by one and the write strobe by two

--- Histogram.hs
+++ Histogram.hs
@@ -4,10 +4,12 @@ import qualified Data.List as L
 histo :: (KnownNat n, KnownNat (2 ^ n), KnownNat b) => Signal (Unsigned n) -> Signal (Unsigned b)
 histo nums = read
   where init = repeat 0
-        delayedWr = False `register` signal True
-        write = 0 `register` (read + 1)
+        delayedWr = toSignal $ delay (False :> False :> Nil) $ signal True
+        --delayedWr' = False `register` signal True
+        write = toSignal $ (0 :> Nil) `delay` fromSignal (read + 1)
         read = blockRamPow2 init nums wrAddr delayedWr write
-        wrAddr = 0 `register` nums
+        --read = asyncRamPow2 nums wrAddr delayedWr write
+        wrAddr = toSignal $ (0 :> 0 :> Nil) `delay` fromSignal nums
 
 -- #### TEST BENCH ####
 
@@ -21,4 +23,5 @@ testInput = stimuliGenerator $ 1 :> 2 :> 2 :> 3 :> 3 :> 3 :> 4 :> 0 :> 1 :> Nil
 expectedOutput :: Signal (Unsigned 10) -> Signal Bool
 expectedOutput = outputVerifier $ 0 :> 0 :> 1 :> 0 :> 1 :> 2 :> 0 :> 0 :> 1 :> Nil
 
-test = L.drop 1 $ sampleN 30 $ expectedOutput (topEntity testInput)
+--test = L.drop 2 $ sampleN 30 $ expectedOutput (topEntity testInput)
+test = L.drop 2 $ sampleN 30 $ (topEntity testInput)
