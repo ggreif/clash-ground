eab308810c8f2d017bcabdde58d4bfc11ff38fe7
ggreif <ggreif@gmail.com>
2015-12-12 00:33:56 +0100

    cleanups

--- Histogram.hs
+++ Histogram.hs
@@ -4,10 +4,10 @@ import qualified Data.List as L
 histo :: (KnownNat n, KnownNat (2 ^ n), KnownNat b) => Signal (Unsigned n) -> Signal (Unsigned b)
 histo nums = read
   where init = repeat 0
-        delayedWr = toSignal $ delay (False :> Nil) $ signal True
-        write = toSignal $ (Nil) `delay` fromSignal (read + 1)
+        delayedWr = False `register` signal True
+        write = read + 1
         read = blockRamPow2 init wrAddr nums delayedWr write
-        wrAddr = toSignal $ (0 :> Nil) `delay` fromSignal nums
+        wrAddr = 0 `register` nums
 
 -- #### TEST BENCH ####
 
@@ -22,5 +22,4 @@ expectedOutput :: Signal (Unsigned 10) -> Signal Bool
 expectedOutput = outputVerifier $ undefined :>
                                0 :> 0 :> 0 :> 1 :> 0 :> 1 :> 1 :> 2 :> 2 :> 0 :> 3 :> 1 :> 4 :> Nil
 
-test = L.drop 1 $ sampleN 15 $ expectedOutput (topEntity testInput)
---test = L.drop 1 $ sampleN 30 $ (topEntity testInput)
+test = L.takeWhile not . L.drop 1 . sample $ expectedOutput (topEntity testInput)
