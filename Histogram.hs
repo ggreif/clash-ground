e4efe9b397f09978cc563550dc056c83924e22f0
Gabor Greif <ggreif@gmail.com>
2015-12-28 17:44:30 +0100

    experiments for read-after-write

--- Histogram.hs
+++ Histogram.hs
@@ -5,9 +5,14 @@ histo :: (KnownNat n, KnownNat (2 ^ n), KnownNat b) => Signal (Unsigned n) -> Si
 histo nums = read
   where init = repeat 0
         wrEnable = False `register` signal True
-        read = blockRamPow2 init wrAddr nums wrEnable write
+        --read = mux (liftA2 (&&) wrEnable same) written $ blockRamPow2 init wrAddr nums wrEnable write
+        read = mux same written $ blockRamPow2 init wrAddr nums wrEnable write
         write = read + 1
-        wrAddr = 0 `register` nums
+        written = 9999 `register` write
+        wrAddr = 1000 `register` nums
+        --same = liftA2 (==) wrAddr (99 `register` nums)
+        --same = liftA2 (==) (99 `register` wrAddr) nums
+        same = liftA2 (==) wrAddr nums
 
 -- #### TEST BENCH ####
 
@@ -15,11 +20,15 @@ topEntity :: Signal (Unsigned 7) -> Signal (Unsigned 10)
 topEntity = histo
 
 
-testInput :: Signal (Unsigned 7)
-testInput = stimuliGenerator $ 1 :> 2 :> 0 :> 2 :> 3 :> 0 :> 3 :> 0 :> 3 :> 4 :> 0 :> 1 :> 0 :> Nil
+testInput, testInput' :: Signal (Unsigned 7)
+testInput' = stimuliGenerator $ 1 :> 2 :> 0 :> 2 :> 3 :> 0 :> 3 :> 0 :> 3 :> 4 :> 0 :> 1 :> 0 :> Nil
+testInput'' = stimuliGenerator $ 0 :> 0 :> 0 :> 0 :> 0 :> 0 :> 0 :> 0 :> 0 :> 0 :> 0 :> 0 :> 0 :> Nil
+testInput = stimuliGenerator $ 0 :> 0 :> 1 :> 1 :> 2 :> 2 :> 3 :> 3 :> 4 :> 4 :> 5 :> 5 :> 0 :> Nil
 
 expectedOutput :: Signal (Unsigned 10) -> Signal Bool
 expectedOutput = outputVerifier $
-                  undefined :> 0 :> 0 :> 0 :> 1 :> 0 :> 1 :> 1 :> 2 :> 2 :> 0 :> 3 :> 1 :> 4 :> Nil
+                --undefined :> 0 :> 0 :> 0 :> 1 :> 0 :> 1 :> 1 :> 2 :> 2 :> 0 :> 3 :> 1 :> 4 :> Nil
+                  ---undefined :> 0 :> 1 :> 2 :> 3 :> 4 :> 5 :> 6 :> 7 :> 8 :> 9 :> 10 :> 11 :> 12 :> Nil
+                  undefined :> 0 :> 1 :> 0 :> 1 :> 0 :> 1 :> 0 :> 1 :> 0 :> 1 :> 0 :> 1 :> 2 :> Nil
 
 test = L.takeWhile not . L.drop 1 . sample $ expectedOutput (topEntity testInput)
