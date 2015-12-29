b2b4ad03547ddc0ee2136b49ff8262c1b21967ed
Gabor Greif <ggreif@gmail.com>
2015-12-29 11:46:12 +0100

    now all tests are as expected!
    the bug was that I did not take wrEn into account

--- Histogram.hs
+++ Histogram.hs
@@ -5,7 +5,7 @@ histo :: (KnownNat n, KnownNat (2 ^ n), KnownNat b) => Signal (Unsigned n) -> Si
 histo nums = read
   where init = repeat 0
         wrEnable = False `register` signal True
-        read = blockRamPow2 init wrAddr nums wrEnable write
+        read = readNew blockRamPow2 init wrAddr nums wrEnable write
         write = read + 1
         wrAddr = 0 `register` nums
 
@@ -15,21 +15,21 @@ topEntity :: Signal (Unsigned 7) -> Signal (Unsigned 10)
 topEntity = histo
 
 
-testInput, testInput' :: Signal (Unsigned 7)
+testInput, testInput', testInput'' :: Signal (Unsigned 7)
 testInput' = stimuliGenerator $ 1 :> 2 :> 0 :> 2 :> 3 :> 0 :> 3 :> 0 :> 3 :> 4 :> 0 :> 1 :> 0 :> Nil
-testInput = pure 0 -- stimuliGenerator $ 0 :> 0 :> 0 :> 0 :> 0 :> 0 :> 0 :> 0 :> 0 :> 0 :> 0 :> 0 :> 0 :> Nil
-testInput'' = stimuliGenerator $ 0 :> 0 :> 1 :> 1 :> 2 :> 2 :> 3 :> 3 :> 4 :> 4 :> 5 :> 5 :> 0 :> Nil
+testInput'' = pure 0
+testInput = stimuliGenerator $ 0 :> 0 :> 1 :> 1 :> 2 :> 2 :> 3 :> 3 :> 4 :> 4 :> 5 :> 5 :> 0 :> Nil
 
 expectedOutput :: Signal (Unsigned 10) -> Signal Bool
 expectedOutput = outputVerifier $
-                  ---undefined :> 0 :> 0 :> 0 :> 1 :> 0 :> 1 :> 1 :> 2 :> 2 :> 0 :> 3 :> 1 :> 4 :> Nil
-                  undefined :> 0 :> 1 :> 2 :> 3 :> 4 :> 5 :> 6 :> 7 :> 8 :> 9 :> 10 :> 11 :> 12 :> Nil
-                  ---undefined :> 0 :> 1 :> 0 :> 1 :> 0 :> 1 :> 0 :> 1 :> 0 :> 1 :> 0 :> 1 :> 2 :> Nil
+                  ---undefined :> 0 :> 0 :> 0 :> 1 :> 0 :> 1 :> 1 :> 2 :> 2 :> 0 :> 3 :> 1 :> 4 :> {- 5 :> -} Nil
+                  ---undefined :> 0 :> 1 :> 2 :> 3 :> 4 :> 5 :> 6 :> 7 :> 8 :> 9 :> 10 :> 11 :> 12 :> Nil
+                  undefined :> 0 :> 1 :> 0 :> 1 :> 0 :> 1 :> 0 :> 1 :> 0 :> 1 :> 0 :> 1 :> 2 :> Nil
 
 test = L.takeWhile not . L.drop 1 . sample $ expectedOutput (topEntity testInput)
 
-readNew :: (Vec m a -> Signal (Unsigned n) -> Signal (Unsigned n) -> Signal Bool -> Signal a -> Signal a) -> Vec m a -> Signal (Unsigned n) -> Signal (Unsigned n) -> Signal Bool -> Signal a -> Signal a
+readNew :: Num a => (Vec m a -> Signal (Unsigned n) -> Signal (Unsigned n) -> Signal Bool -> Signal a -> Signal a) -> Vec m a -> Signal (Unsigned n) -> Signal (Unsigned n) -> Signal Bool -> Signal a -> Signal a
 readNew ram ini wrAd rdAd wrEn wrData = mux wasSame wasWritten $ ram ini wrAd rdAd wrEn wrData
   where sameAd = liftA2 (==) wrAd rdAd
-        wasSame = False `register` sameAd
-        wasWritten = error "HEX!" `register` wrData
+        wasSame = False `register` (liftA2 (&&) wrEn sameAd)
+        wasWritten = 0 `register` wrData
