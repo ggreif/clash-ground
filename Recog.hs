48c1cf95a72c70548eb2b33983f51d56fcae72da
Gabor Greif <ggreif@gmail.com>
2015-12-30 12:44:24 +0100

    clean up

--- Recog.hs
+++ Recog.hs
@@ -1,21 +1,15 @@
-{-# LANGUAGE PartialTypeSignatures #-}
-
 module Recog where
 
 import CLaSH.Prelude
-import Debug.Trace
+--import Debug.Trace
 
 recog :: (KnownNat (d + 1 + m), KnownNat m) => SNat (d + 1) -> BitVector m -> Signal (BitVector (d + 1 + m)) -> Signal (Maybe (Unsigned 6))
 recog d pat s = fold (liftA2 find) (zipWith match poss shifts)
   where find a@Just{} _ = a
         find _ b = b
-        --comps :: Vec (d + 1) (Signal )
         shifts = iterate d (`shiftR` 1) s
-        --poss = indices d --iterate d (+1) 0
-        --poss :: Vec _ (Unsigned 6)
         poss = iterate d (+1) 0
-        --match :: Unsigned 6 -> Signal (BitVector _) -> Signal (Maybe (Unsigned 6))
-        match pos shifted = (\sig -> if traceShow (sig, pat) truncateB sig == pat then Just pos else Nothing) <$> shifted
+        match pos shifted = (\sig -> if truncateB sig == pat then Just pos else Nothing) <$> shifted
 
 
 -- #### TEST BENCH ####
