936056ad7510a84e96d6e5b195cbcdd63b04ff63
Gabor Greif <ggreif@gmail.com>
2015-12-30 12:51:37 +0100

    match 3 bits only
    use traces to see that we matched 5 bits

--- Recog.hs
+++ Recog.hs
@@ -1,7 +1,7 @@
 module Recog where
 
 import CLaSH.Prelude
---import Debug.Trace
+import Debug.Trace
 
 recog :: (KnownNat (d + 1 + m), KnownNat m) => SNat (d + 1) -> BitVector m -> Signal (BitVector (d + 1 + m)) -> Signal (Maybe (Unsigned 6))
 recog d pat s = fold (liftA2 find) (zipWith match poss shifts)
@@ -9,17 +9,17 @@ recog d pat s = fold (liftA2 find) (zipWith match poss shifts)
         find _ b = b
         shifts = iterate d (`shiftR` 1) s
         poss = iterate d (+1) 0
-        match pos shifted = (\sig -> if truncateB sig == pat then Just pos else Nothing) <$> shifted
+        match pos shifted = (\sig -> if traceShow (sig, pat) truncateB sig == pat then Just pos else Nothing) <$> shifted
 
 
 -- #### TEST BENCH ####
 
 topEntity :: Signal (BitVector 8) -> Signal (Maybe (Unsigned 6))
-topEntity = recog d3 0b101
+topEntity = recog d5 0b101 -- check 3 bits
 
 
 testInput :: Signal (BitVector 8)
-testInput = pure 0b1010
+testInput = pure 0b1011010
 
 expectedOutput :: Signal (Maybe (Unsigned 6)) -> Signal Bool
 expectedOutput = outputVerifier $ Just 0 :> Just 0 :> Nil
