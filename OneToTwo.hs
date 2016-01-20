7c2afb7205ec00217a0cce13ce1f5c0d3db96287
Gabor Greif <ggreif@gmail.com>
2016-01-20 17:03:53 +0100

    hook up with selectV3

--- OneToTwo.hs
+++ OneToTwo.hs
@@ -7,10 +7,11 @@ encode1x2 :: FiniteBits d => Signal (Vec 2 d) -> Signal (Vec 3 d)
 encode1x2 = bundle . (\(a:>b:>Nil)-> a:>b:>(a `xor` b):>Nil) . unbundle
 
 
-decode1x2 :: FiniteBits d => Signal (Vec 2 (Maybe d)) -> Signal (Maybe (Vec 2 d, Bool))
-decode1x2 = undefined
-  where select (Just a) (Just b) (Just a'b) = Just (a:>b:>Nil, a `xor` b == a'b)
-        select (Just a) (Just b) Nothing = Just (a:>b:>Nil, True)
-        select (Just a) Nothing (Just a'b) = Just (a:>a `xor`a'b:>Nil, True)
-        select Nothing (Just b) (Just a'b) = Just (b `xor`a'b:>b:>Nil, True)
+decode1x2 :: FiniteBits d => Signal (Vec 3 (Maybe d)) -> Signal (Maybe (Vec 2 d, Bool))
+decode1x2 = selectV3 . unbundle
+  where select (Just a) (Just b) extra = Just (a:>b:>Nil, case extra of Just a'b -> a `xor` b == a'b; Nothing -> True)
+        select (Just a) Nothing (Just a'b) = Just (a :> a `xor` a'b :> Nil, True)
+        select Nothing (Just b) (Just a'b) = Just (a'b `xor` b :> b:> Nil, True)
         select _ _ _ = Nothing
+        selectV3 :: FiniteBits d => Vec 3 (Signal (Maybe d)) -> Signal (Maybe (Vec 2 d, Bool))
+        selectV3 (a:>b:>a'b:>Nil) = select <$> a <*> b <*> a'b
