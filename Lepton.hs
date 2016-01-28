0a0488664257c9a729fcc93216318a5d3507dfd3
Gabor Greif <ggreif@gmail.com>
2016-01-28 10:04:22 +0100

    add variables and implement the evaluator
    NEXT: abstract machine

--- Lepton.hs
+++ Lepton.hs
@@ -9,7 +9,8 @@ class Lam f where
   lam :: (f a -> f b) -> f (a -> b)
   app :: f (a -> b) -> f a -> f b
 
-
+class Val f where
+  int :: Int -> f Int
 
 
 data Baryon a where
@@ -22,9 +23,24 @@ instance Lam Baryon where
   lam = Barylam
   app = Baryapp
 
+instance Val Baryon where
+  int = BaryInt
+
 -- Here is our standard evaluator:
 --
 evalB :: Baryon a -> a
 evalB (f `Baryapp` a) = evalB f $ evalB a
+evalB (BaryVar v) = v
 evalB (BaryInt i) = i
 evalB (Barylam f) = evalB . f . BaryVar
+
+
+test :: (Lam f, Val f) => f Int
+test = id `app` (const `app` fortytwo `app` seven)
+  where id = lam (\x->x)
+        const = lam (\x->lam(\_->x))
+        fortytwo = int 42
+        seven = int 7
+
+t0 :: Baryon Int
+t0 = test
