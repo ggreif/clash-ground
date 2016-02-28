0b4d2c8aeb7a2f5e8b6ad9a331952335532fa018
ggreif <ggreif@gmail.com>
2016-02-28 15:42:40 +0100

    WIP: cleaned out some blind avenues

--- Lepton.hs
+++ Lepton.hs
@@ -7,14 +7,12 @@ import GHC.Exts
 import Debug.Trace (trace, traceShow, traceShowId)
 import Data.Type.Equality
 import qualified Data.List as L
---import Unsafe.Coerce (unsafeCoerce)
 
 data Dict :: Constraint -> * where
   Dict :: c => Dict c
 
 
 class Lam f where
-  --lam :: ((forall i . (DeBruijnIndex i (a ': s)) => f (a ': i)) -> f (b ': a ': s)) -> f ((a -> b) ': s)
   lam :: ((forall i . (TRUNC (Trunc '[] (a ': s) i) (a ': s) i) => f (a ': i)) -> f (b ': a ': s)) -> f ((a -> b) ': s)
   app :: f ((a -> b) ': s) -> f (a ': s) -> f (b ': s)
 
@@ -32,9 +30,6 @@ data Baryon s where
   Baryapp :: Baryon ((a -> b) ': s) -> Baryon (a ': s) -> Baryon (b ': s)
   BaryInt :: Int -> Baryon (Int ': s)
   BaryVar :: a -> Baryon (a ': s)
-  --BaryBruijn :: (DbIndex (a ': s) s' ~ idx, Consume a idx s', Builds idx) => CONT ((a -> b) ': s') k -> Baryon (a ': s)
-  --BaryBruijn :: (DeBruijnIndex s (a ': s')) => CONT ((a -> b) ': s') k -> Baryon (a ': s)
-  --BaryBruijn :: (TRUNC idx (a ': s') s) => CONT ((a -> b) ': s') k -> Baryon (a ': s)
   BaryBruijnX :: TRUNC idx (a ': s') s => CONT (b ': a ': s') k -> Baryon (a ': s)
 
 instance Lam Baryon where
@@ -61,7 +56,6 @@ instance Show (Baryon s) where
   show (Baryapp f a) = "Baryapp (" L.++show f L.++") (" L.++show a L.++")"
   show (BaryInt i) = show i
   show (BaryVar _) = "<var>"
-  --show (BaryBruijn cont) = "PPP " L.++ show cont
   show (BaryBruijnX cont) = "PPX " L.++ show cont
   show (BaryPush f x) = "BaryPush f"
 
@@ -117,7 +111,7 @@ eval' (C1 c) (Barylam f) | traceShow ("C1bruijnX", show c) True = eval' c (f (Ba
 
 
 
-eval' c'@(CENTER x c'') (Barylam f) | traceShow ("CENTERbruijnX!!", show c'') True = eval' c'' (BaryPush f x) --(BaryVar $ \a -> evalB (f (BaryVar a)))
+eval' c'@(CENTER x c'') (Barylam f) | traceShow ("CENTERbruijnX!!", show c'') False = eval' c'' (BaryPush f x) --(BaryVar $ \a -> evalB (f (BaryVar a)))
 -- exec c (\a -> evalB (f (BaryVar a)))
 
 -- SCETCH: BaryPush (evalB (f (BaryVar a))) === (BaryVar $ \a -> evalB (f (BaryVar a)))
@@ -193,21 +187,10 @@ eval' c (BaryVar v) = exec c v
 eval' c (BaryInt i) = exec c i
 {- ^^ expand evalB -}
 
---eval' c (BaryBruijn (C1 c')) | traceShow ("@@", show c', show c) True = exec c (trunc (x undefined extract c') (x undefined extract c))
---  where x :: a -> (a -> b) -> a -> b
---        x _ f a = f a
---eval' c@(CDROP (CENTER _ _)) (BaryBruijn c'@(CENTER _ _)) | traceShow ("@@@", show c', show c) True = exec c (trunc (extract c') (extract c))
 
---eval' (CDROPX c) (BaryBruijnX (CDROPX c')) | traceShow ("@@X", show c', show c) True = exec c (trunc (extract c') (extract c))
 eval' c (BaryBruijnX c') | traceShow ("@@X", show c', show c) True = exec c (trunc (extract c') (extract c))
 
 
-{-
-eval' c (BaryBruijn c') | traceShow ("@@", show c', show c) True = exec c (grab c' c)
-  where grab :: Consume a s s' => CONT ((a -> b) ': s') k' -> CONT (a ': s) k -> a
-        grab (C1 (CENTER a _)) _ = a
-        grab shallow deep = peelC (extract shallow) deep
--}
 eval' c e = exec c (eval (traceShow ("EVAL:::", c, e) e))  -- (OWK)
 
 
@@ -227,105 +210,7 @@ type family EqZip (deep :: [*]) (shallow :: [*]) :: Constraint where
   EqZip (a ': peel) '[a] = ()
   EqZip (a ': as) (a ': bs) = EqZip as bs
 
--- AVENUE B
--- DeBruijnIndex [f, e, d, c, b, a] [c, b, a] = Consume [f, e, d, c (, ...)] 
--- :kind! DeBruijnIndex '[Float, Either Int Int, Double, Char, Bool, Int] '[Char, Bool, Int]
-
-{-
-type family DBI (odeep :: [*]) (oshallow :: [*])(dacc :: [*]) (sacc :: [*]) (deep :: [*]) (shallow :: [*]) :: Constraint where
-  DBI orig oshall dacc sacc (d ': deep) (s ': shallow) = DBI orig oshall (d ': dacc) (s ': sacc) deep shallow
-  DBI orig oshall dacc sacc (d ': deep) '[] = DBI orig oshall (d ': dacc) sacc deep '[]
-  DBI orig oshall (d ': dacc) '[s] '[] '[] = (d ~ s, Consume d (Reverse dacc) orig, Reverse dacc ~ DbIndex oshall orig)
-  DBI orig oshall (d ': dacc) (s ': sacc) '[] '[] = (d ~ s, DBI orig oshall dacc sacc '[] '[])
-
---type DeBruijnIndex deep shallow = DBI deep shallow '[] '[] deep shallow
-class DBI deep shallow '[] '[] deep shallow => DeBruijnIndex deep shallow where
-  peelX :: (shallow ~ (a ': sh)) => DB shallow -> DB deep -> a
-instance DBI deep (a ': shallow) '[] '[] deep (a ': shallow) => DeBruijnIndex deep (a ': shallow) where
-  peelX (TCons a _) _ = a
-
-type family (idx ::[*]) ++ (shallow :: [*]) :: [*] where
-  '[] ++ shallow = shallow
-  (a ': idx) ++ shallow = a ': idx ++ shallow
-
-class Builds (idx ::[*]) where
-  rev :: DB idx
-
-instance Builds '[] where
-  rev = Lepton.Nil
-
-instance Builds as => Builds (a ': as) where
-  rev = TCons undefined rev
-
-class Builds rev => Consume d (rev :: [*]) (deep :: [*]) where
-  peel :: DB rev -> DB deep -> d
-  peelC :: DB rev -> CONT (t ': deep) k -> d
-  peelC db (extract -> dbd) = peel db dbd
-
-instance Consume a '[] (a ': sha) where
-  peel _ (TCons a _) = a
-
-instance ({-(ds ++ (a ': sha)) ~ deeps, -}Consume a ds deeps) => Consume a (d ': ds) (d ': deeps) where
-  peel (TCons _ rest0) (TCons _ rest) = peel rest0 rest
--}
-
--- AVENUE C
-{-
-type family INDEX (dacc :: [*]) (sacc :: [*]) (deep :: [*]) (shallow :: [*]) :: [*] where
-  INDEX dacc sacc (d ': deep) (s ': shallow) = INDEX (d ': dacc) (s ': sacc) deep shallow
-  INDEX dacc sacc (d ': deep) '[] = INDEX (d ': dacc) sacc deep '[]
-  INDEX (d ': dacc) '[s] '[] '[] = Reverse dacc
-  INDEX (d ': dacc) (s ': sacc) '[] '[] = INDEX dacc sacc '[] '[]
-
-type DbIndex shallow deep = INDEX '[] '[] deep shallow
-
-class Builds (DbIndex shallow deep) => LevelDiff (shallow :: [*]) (deep :: [*]) where
-  type Idx shallow deep :: [*]
-  type Idx shallow deep = DbIndex shallow deep
--}
-
---instance (shallow ~ deep) => LevelDiff shallow deep where
---  type Idx shallow deep = '[]
-
---instance LevelDiff shallow deep => LevelDiff shallow (d ': deep) where
---  --type Idx shallow (d ': deep) = d ': Idx shallow deep
-
--- AVENUE D
-
--- Sketch:
---    Map (==shallow, etc.) (Tails deep)  ---> [(False, shallow, deep), (False, shallow, d1), (True, sh, dn), ...]
-
-{-
-type family EqList (a :: [*]) (b :: [*]) where
-  EqList a a = 'True
-  EqList a b = 'False
-type instance a == b = EqList a b
--}
-{-
-data Bool' b where
-  T :: Bool' True
-  F :: Bool' False
-
-class Boolable (b :: Bool) where
- theBool :: Bool' b
-
-instance Boolable True where theBool = T
-instance Boolable False where theBool = F
-
-
-class (Indexable' (shallow == deep) shallow deep) => Indexable (shallow :: [*]) (deep :: [*])
-instance Indexable' (shallow == deep) shallow deep => Indexable (shallow :: [*]) (deep :: [*])
-
-class Boolable same => Indexable' (same :: Bool) (shallow :: [*]) (deep :: [*]) where
-  index :: ((a ': sh) ~ shallow) => DB shallow -> DB deep -> a
-
-instance Indexable' True (a ': shallow) (a ': shallow) where
-  index _ (TCons a _) = a
-
-
-instance Indexable' (a ': shallow == deep) (a ': shallow) deep => Indexable' False (a ': shallow) (d ': deep) where
-  index shallow@TCons{} (TCons _ deep) = undefined -- index shallow deep
--}
+-- all other avenues, see git history
 
 -- AVENUE E
 
@@ -359,10 +244,6 @@ instance ((i ': indx) ~ Trunc '[] (a ': shallow) (d ': deep), TRUNC indx (a ': s
   trunc sh (TCons _ rest) = trace "DRILLING" $ trunc sh rest
 
 
---class TRUNC index shallow deep => TRUNC' index shallow deep | shallow deep -> index
-
---instance (index ~ Trunc '[] shallow deep, TRUNC index shallow deep) => TRUNC' index shallow deep
-
 
 data CONT :: [*] -> * -> * where
   C0 :: Baryon ((a -> b) ': s) -> !(CONT (b ': s) k) -> CONT (a ': s) k
@@ -371,7 +252,6 @@ data CONT :: [*] -> * -> * where
   --CENTER :: !(CONT (b ': s) k) -> CONT (b ': a ': s) k
   CENTER :: a -> !(CONT (b ': s) k) -> CONT (b ': a ': s) k
   CDROPX :: !(CONT ((a' -> b) ': s) k) -> CONT (b ': a ': s) k
---  C2 :: CONT (b2 ': s3) k -> CONT (b2 ': y ': x ': s3) k
 
   CHALT :: CONT '[a] a
 
@@ -381,7 +261,7 @@ instance Show (CONT (a ': s) k) where
   show (C1 (CENTER _ c)) = "(1^)" L.++ show c
   show (C1 c) = '1' : show c
   show (CENTER a c) = '^' : show c
-  show (CDROPX c) = 'V' : show c
+  show (CDROPX c) = 'X' : show c
 
 extract :: CONT (a ': ctx) k -> DB ctx
 extract (C1 (CENTER _ c)) = extract c -- these neutralize
@@ -395,8 +275,6 @@ extract (CDROPX c) = TCons (error $ show ("CDROPX", c)) $ extract c
 
 
 exec :: CONT (a ': s) k -> a -> k
-
---exec (C2 c) !f = exec c f
 
 exec (CDROPX c) !f = exec c (const f)
 
