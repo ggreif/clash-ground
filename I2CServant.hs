2e526f0555773bc114f53e7f70081f5632877bbe
Gabor Greif <ggreif@gmail.com>
2016-06-11 12:01:17 +0200

    sketch how pca9552 should be wired up
    internally, using bitSlave

--- I2CServant.hs
+++ I2CServant.hs
@@ -4,20 +4,21 @@ module I2C'Servant where
 
 import CLaSH.Prelude
 
-class Serve a where
+class Serve (a :: Bool -> * -> *) where
   type Next a
   accept :: Unsigned 8 -> Maybe (Next a)
 
 
 data Done
 
-data left <|> right
+data (left :: Bool -> * -> *) <|> (right :: Bool -> * -> *) :: Bool -> * -> * where
 
 instance (Serve left, Serve right) => Serve (left <|> right) where
   type Next (left <|> right) = Next left `Either` Next right
   accept recvd = undefined -- case (accept :: Unsigned 8 -> Maybe (Next left)) recvd of
                  --  Just l -> Just (Left l)
 
+{-
 data Address (addr :: Nat) next
 
 instance (KnownNat addr, Serve next) => Serve (Address addr next) where
@@ -37,7 +38,7 @@ type SendOne = SendBytes 1
 -- We whould automatically ack.
 --  The type won't contain this junk
 -- data AckReceived
-
+-}
 
 -- ** Signatures
 
@@ -50,10 +51,20 @@ flankDetect = undefined
 
 bitSlave :: Signal ((Bit, Bool), (Bit, Bool)) -- SDA, SCL + flanks
          -> Signal (Unsigned 8) -- byte to write
-         -> Signal (Unsigned 8, (Bool, Bool, Bool), Bit) -- byte read, (START, ACK, NACK, ReSTART), SDA-out
+         -> Signal Bool -- ACK-out
+         -> Signal (Unsigned 8, (Bool, Bool, Bool, Bool), Bit) -- byte read, (START, ACK, NACK, ReSTART), SDA-out
 bitSlave = undefined
 
 -- ** Example: PCA9552
 
-type PCA9552 = Address 0b1100000 (ReadOne Done)
+--type PCA9552 = Address 0b1100000 (ReadOne Done)
 
+type State = (Unsigned 8, Vec 8 (Unsigned 8))
+
+pca9552 :: Signal ((Bit, Bool), (Bit, Bool)) -- SDA, SCL + flanks
+        -> Signal Bit                        -- SDA-out
+pca9552 i = o
+  where (read, conds, o) = unbundle $ bitSlave i write ack
+        (ack, write) = mealyB step (0, CLaSH.Prelude.repeat 0b01010101) (read, conds)
+        step :: State -> (Unsigned 8, (Bool, Bool, Bool, Bool)) -> (State, (Bool, Unsigned 8))
+        step s i = (s, (True, 0)) -- FIXME
