5a5052ba91b2b29a5a06ecf4170ad12340168b28
Gabor Greif <ggreif@gmail.com>
2016-07-23 13:32:15 +0200

    junk insertion

--- Item.hs
+++ Item.hs
@@ -13,20 +13,22 @@ data Wagon where
   Abstractor :: (BitVector 10) -> Wagon
   Hole :: Wagon
   Stop :: Wagon
+  Junk :: Wagon
  deriving Show
 
 type Train = (Vec 20 Wagon, Vec 20 (Vec 10 (Maybe Int)))
 
 eval :: Train -> () -> (Train, Bool)
 eval (ws, is) _ = traceShowId ((ws', is'), done ws')
-  where pairing = zipWith exam ws (left ws)
+  where pairing = zipWith3 exam is ws (left ws)
         left (ws :> tail) = tail :< ws
         done (Stop :> _) = True
         done _ = False
-        ws' = left ws
-        is' = left is
+        ws' = left (fst.fst <$> pairing)
+        is' = left (snd <$> pairing)
         
-        exam (Applicator i) (Abstractor bs) = (\case True -> Just i; _ -> Nothing) <$> (unpack bs :: Vec 10 Bool)
+        exam _ (Applicator i) (Abstractor bs) = ((Junk, Junk), bool Nothing (Just i) <$> (unpack bs :: Vec 10 Bool))
+        exam is w n  = ((w, n), is)
 
 meval = mealy eval start
   where start = (repeat (Applicator 1) :< Abstractor 0b11 :< Hole :< Hole :< Stop, repeat $ repeat Nothing)
