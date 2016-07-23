f065cf2010fffb59bd212bbaf9afa4a6dfc74a32
Gabor Greif <ggreif@gmail.com>
2016-07-23 13:16:35 +0200

    it rotates!

--- Item.hs
+++ Item.hs
@@ -1,8 +1,11 @@
-{-# language GADTSyntax #-}
+{-# language GADTSyntax, LambdaCase #-}
+
 module Item where
 
 import CLaSH.Prelude
 import qualified Data.List as L
+import Data.Bool
+import Debug.Trace
 
 
 data Wagon where
@@ -10,11 +13,20 @@ data Wagon where
   Abstractor :: (BitVector 10) -> Wagon
   Hole :: Wagon
   Stop :: Wagon
-  
-type Train = (Vec 20 Wagon, Vec 20 (Vec 10 Int))
+ deriving Show
+
+type Train = (Vec 20 Wagon, Vec 20 (Vec 10 (Maybe Int)))
 
 eval :: Train -> () -> (Train, Bool)
-eval (ws, is) _ = undefined
+eval (ws, is) _ = traceShowId ((ws', is'), done ws')
+  where pairing = zipWith exam ws (left ws)
+        left (ws :> tail) = tail :< ws
+        done (Stop :> _) = True
+        done _ = False
+        ws' = left ws
+        is' = left is
+        
+        exam (Applicator i) (Abstractor bs) = (\case True -> Just i; _ -> Nothing) <$> (unpack bs :: Vec 10 Bool)
 
 meval = mealy eval start
-  where start = (repeat $ Applicator 1, repeat $ repeat 0)
+  where start = (repeat (Applicator 1) :< Abstractor 0b11 :< Hole :< Hole :< Stop, repeat $ repeat Nothing)
